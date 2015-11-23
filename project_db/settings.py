7a0e08ae8cb6912392dc3c25ccd1bd9253f69591
Alejandro <alejandro@herce.me>
2015-11-23 04:18:39 +0000

    Listo para Heroku

--- project_db/settings.py
+++ project_db/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'project_db.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
