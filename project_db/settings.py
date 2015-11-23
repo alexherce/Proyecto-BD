f69d14d0bafd9c27594ae17b4bd8b1b7b55525b3
Alejandro <alejandro@herce.me>
2015-11-23 04:09:51 +0000

    Listo para Heroku

--- project_db/settings.py
+++ project_db/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -93,14 +94,9 @@ ON_HEROKU = os.environ.get('ON_HEROKU')
 
 if ON_HEROKU == '1':
     import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+    DATABASES['default'] =  dj_database_url.config()
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+   DATABASES['default'] =  dj_database_url.config()
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
